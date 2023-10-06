ae435c86c3e577cf2f80248695ab0f270a6709f9
Milen Tecle <m_tecle@hotmail.com>
2023-10-06 13:38:49 +0000

    Toggle sound on click and change fontawesome icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
+
+// Toggles function for sound icons when clicked upon
+const audio = new Audio('assets/audio/heartbeat-sound.mp3');
+const soundOn = document.getElementById('sound-on');
+const soundOff = document.getElementById('sound-off');
+
+function toggleSound() {
+    if (audio.paused) {
+        audio.play();
+        soundOn.style.display = ('inline-block');
+        soundOff.style.display = ('none');
+    } else {
+        audio.pause();
+        soundOn.style.display = ('none');
+        soundOff.style.dispaly = ('inline-block');
+    }
+}
+
+// Click event listenters to sound icons
+soundOn.addEventListener('click', toggleSound);
+soundOff.addEventListener('click', toggleSound);
+
+// Hides the sound-on icon
+soundOn.style.display = 'none';
+
+
+
+// Quiz questions
 const questions = [
     {
         question: "What is the largest organ in the human body?",
@@ -11,4 +39,5 @@ const questions = [
 
 ]
 
-const audio = new Audio('assets/audio/heartbeat-sound.mp3');
+
+
