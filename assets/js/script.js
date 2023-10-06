0e73ec88b2d57693622643e3294458347c9daf20
Milen Tecle <m_tecle@hotmail.com>
2023-10-06 09:24:47 +0000

    Create file for audio and add first question in JS-file

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const questions = [
+    {
+        question: "What is the largest organ in the human body?",
+        answers: [
+            { text: "Heart", correct: false},
+            { text: "Lungs", correct: false},
+            { text: "Skin", correct: true},
+            { text: "Liver", correct: false},
+        ]
+    },
+    
+]
